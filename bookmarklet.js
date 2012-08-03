9eed2555a27a35a6839e65d8b4b3021017c99342
Peter Jaric <peter@jaric.org>
2012-08-03 22:20:16 +0200

    getScript -> getScripts, sea surfer lists internal links and escapes quotes

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,32 +1,37 @@
 (function(){
-    var repo = 'https://raw.github.com/peterjaric/seasurfer';
-    var branch = 'v3';
-
+    var w = window.open();
+    getScripts(['http://code.jquery.com/jquery-1.7.2.min.js', 
+		'http://localhost:8080/seasurfer.js'],
+//		'https://raw.github.com/peterjaric/seasurfer/v3/seasurfer.js'],
+	       function() {
+		   // Remove all global traces of our newly loaded
+		   // jQuery and then run the seaSurfer
+		   jQuery.noConflict(true)(function(jQuery) {
+		       seaSurfer(jQuery, w, location.hostname);
+		   });
+	       });
+    
     // getScript() by paul irish
     // http://pastie.org/462639
-    function getScript(url, success){
-        var head = document.getElementsByTagName("head")[0],
-        done = false;
-        var script = document.createElement("script");
-        script.src = url;
-         
+    // modified to load many scripts
+    function getScripts(urls, success){
+        var head = document.getElementsByTagName('head')[0],
+            done = false,
+            script = document.createElement('script');
+	script.src = urls.shift();
+
         // Attach handlers for all browsers
-        script.onload = script.onreadystatechange = function() {
-            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                done = true;
-                success();
-            }
-        };
+        script.onload = script.onreadystatechange = 
+	    function() {
+		if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                    done = true;
+		    if (urls.length === 0 ) { 
+			success();
+		    } else {
+			getScripts(urls, success);
+		    }
+		}
+            };
         head.appendChild(script);
     }
-
-    getScript('http://code.jquery.com/jquery-1.7.2.min.js', function() {
-        // Remove all global traces of our newly loaded
-        // jQuery and then run the seaSurfer
-        jQuery.noConflict(true)(function(jQuery) {
-	    getScript(repo + '/' + branch + '/seasurfer.js', function() {
-		seaSurfer(jQuery);
-            });
-        });
-    });
 })();
--- seasurfer.js
+++ seasurfer.js
@@ -1,29 +1,54 @@
-function seaSurfer($) {
-    var version = '3';
-
-    function convertToHidden(elem) {
-        return '<input name="' + elem.attr('name') + '" value="' + elem.val() + '" type="hidden"/>';
+function seaSurfer($, w, hostname) {
+    function createSimpleInput(inputElem) {
+	var name = inputElem.attr('name');
+	// Escape quotes to avoid breaking out of the value attribute when looking for XSS vulns
+	var value = inputElem.val().replace('"', '&quot;');
+        return '<input name="' + name + '" value="' + value + '" type="hidden"/>';
     }
     
-    var body = $(w.document.body);
+    var version = '3',
+        body = $(w.document.body),
+        links,
+        foundLinks = false;
+
     body.append('<h1>Sea Surfer v' + version + '</h1><p>By <a href="http://twitter.com/peterjaric">@peterjaric</a> at <a href="http://javahacker.com">javahacker.com</a>.</p>');
     
-    // Create test entry for each form on the page
+    // Create a test entry for each internal link on the page
+    links = $('<div id="links">');
+    $('a').each(function(i) {
+	if (this.hostname === hostname && this.search) {
+	    foundLinks = true;
+            // Prepare the form code
+            var submit, input = $('<input id="link' + i + '" type="text" size="160">');
+	    input.val(this.href);
+            links.append(input);
+            script = 'javascript:var input = document.getElementById(\'link' + i + '\'); window.open(input.value);';
+            links.append('<input type=submit value="Test vulnerability" onclick="' + script + '"/><br/>');
+	}
+    });
+    if (foundLinks) {
+	body.append('<p id="showLinks"><a onclick="javascript:document.getElementById(\'links\').style.display=\'block\'; document.getElementById(\'showLinks\').style.display=\'none\';">Click to show internal links</a></p>');
+	body.append(links);
+	links.hide();
+    }
+
+    // Create a test entry for each form on the page
     $('form').each(function(i) {
-        var form, formInputs, inputs, area, desc, script, a, autoSubmit, formHtml;
+        var form, formInputs, inputs, area, desc, script, a, autoSubmit, formHtml, action;
         form = $(this).clone();
-        
-        // Some info about the form
-        desc = '<h2>Form ' + (i + 1) + ' (' + form.attr('action') + ')</h2>';
-        body.append(desc);
-	
+        action = form.attr('action') || ''
+
         // Prepare the form code
         a = document.createElement('a');
-        a.href = form.attr('action');
+        a.href = action;
         form.attr('action', a.href);
         form.removeAttr('onsubmit');
         form.removeAttr('class');
         form.removeAttr('style');
+
+        // Some info about the form
+        desc = '<h2>Form ' + (i + 1) + ' (' + a.href + ')</h2>';
+        body.append(desc);
 	
         // Find the current inputs (etc) of the form, only caring about the current values
         formInputs = 'input[type=text],input[type=hidden],input[type=password],input:checked,select,textarea';
@@ -33,28 +58,19 @@ function seaSurfer($) {
         form.empty();
         form.append('\n');
         inputs.each(function() {
-            console.log($(this).val());
-            form.append('  ' + convertToHidden($(this)) + '\n');
+            form.append('  ' + createSimpleInput($(this)) + '\n');
         });
-	
+
         // Create a textarea and insert the form and some code that will auto submit the form later
         area = $('<textarea id=\'area' + i + '\' cols=120 rows=20>');
         autoSubmit = '<script>document.getElementsByTagName(\'form\')[0].submit();</script>';
         formHtml = form.wrap('<div>').parent().html();
-        area.text(formHtml + autoSubmit);
+        area.text(formHtml + '\n' + autoSubmit);
         body.append(area);
-	
+
         // Create a button that will open a new window that will submit the form
-        script = 'javascript:function insertAfter(newChild,refChild){refChild.parentNode.insertBefore(newChild,refChild.nextSibling);} var area = document.getElementById(\'area' + i + '\'); var csrfWin = window.open();  csrfWin.document.write(area.value);';
+        script = 'javascript:var area = document.getElementById(\'area' + i + '\'); var csrfWin = window.open();  csrfWin.document.write(area.value);';
         body.append('<br/><input type=submit value="Test vulnerability" onclick="' + script + '"/><br/>');
     });
 }
 
-
-getScript('http://code.jquery.com/jquery-1.7.2.min.js', function() {
-    // Remove all global traces of our newly loaded
-    // jQuery and Then run the seaSurfer
-    jQuery.noConflict(true)(function(jQuery) {
-        seaSurfer(jQuery);
-    });
-});
